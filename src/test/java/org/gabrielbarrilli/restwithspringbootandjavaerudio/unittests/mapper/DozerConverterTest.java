7ea4443d93ecaca74028f12081512c21a2bbb519
Barrilli01 <gabrielbarrillicruz@gmail.com>
2024-01-30 21:35:36 -0300

    Test "consertado"

--- src/test/java/org/gabrielbarrilli/restwithspringbootandjavaerudio/unittests/mapper/DozerConverterTest.java
+++ src/test/java/org/gabrielbarrilli/restwithspringbootandjavaerudio/unittests/mapper/DozerConverterTest.java
@@ -32,31 +32,39 @@ public class DozerConverterTest {
 
     @Test
     public void parseEntityListToVOListTest() {
-        List<PersonVO> outputList = DozerMapper.parseListObjects(inputObject.mockEntityList(), PersonVO.class);
-        PersonVO outputZero = outputList.get(0);
-        
-        assertEquals(Long.valueOf(0L), outputZero.getId());
-        assertEquals("First Name Test0", outputZero.getFirstName());
-        assertEquals("Last Name Test0", outputZero.getLastName());
-        assertEquals("Addres Test0", outputZero.getAddress());
-        assertEquals("Male", outputZero.getGender());
-        
-        PersonVO outputSeven = outputList.get(7);
-        
-        assertEquals(Long.valueOf(7L), outputSeven.getId());
-        assertEquals("First Name Test7", outputSeven.getFirstName());
-        assertEquals("Last Name Test7", outputSeven.getLastName());
-        assertEquals("Addres Test7", outputSeven.getAddress());
-        assertEquals("Female", outputSeven.getGender());
-        
-        PersonVO outputTwelve = outputList.get(12);
-        
-        assertEquals(Long.valueOf(12L), outputTwelve.getId());
-        assertEquals("First Name Test12", outputTwelve.getFirstName());
-        assertEquals("Last Name Test12", outputTwelve.getLastName());
-        assertEquals("Addres Test12", outputTwelve.getAddress());
-        assertEquals("Male", outputTwelve.getGender());
-    }
+        List<Person> inputList = inputObject.mockEntityList();
+
+        if(!inputList.isEmpty()) {
+            List<PersonVO> outputList = DozerMapper.parseListObjects(inputList, PersonVO.class);
+            PersonVO outputZero = outputList.get(0);
+
+            assertEquals(Long.valueOf(0L), outputZero.getId());
+            assertEquals("First Name Test0", outputZero.getFirstName());
+            assertEquals("Last Name Test0", outputZero.getLastName());
+            assertEquals("Addres Test0", outputZero.getAddress());
+            assertEquals("Male", outputZero.getGender());
+
+            PersonVO outputSeven = outputList.get(7);
+
+            assertEquals(Long.valueOf(7L), outputSeven.getId());
+            assertEquals("First Name Test7", outputSeven.getFirstName());
+            assertEquals("Last Name Test7", outputSeven.getLastName());
+            assertEquals("Addres Test7", outputSeven.getAddress());
+            assertEquals("Female", outputSeven.getGender());
+
+            PersonVO outputTwelve = outputList.get(12);
+
+            assertEquals(Long.valueOf(12L), outputTwelve.getId());
+            assertEquals("First Name Test12", outputTwelve.getFirstName());
+            assertEquals("Last Name Test12", outputTwelve.getLastName());
+            assertEquals("Addres Test12", outputTwelve.getAddress());
+            assertEquals("Male", outputTwelve.getGender());
+        } else {
+            System.out.println("A lista está vazia!");
+
+        }
+
+        }
 
     @Test
     public void parseVOToEntityTest() {
